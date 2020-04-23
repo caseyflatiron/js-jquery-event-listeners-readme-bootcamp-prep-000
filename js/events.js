f54183bed591a6241cdd7c8d45ac2b1e0b87ae89
casey bradford <caseyebradford@gmail.com>
2020-04-23 21:00:51 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,14 @@ function frameIt() {
   })
 }
 
+function pressIt() {
+  $('input').on("keydown", function(key){
+    if(key.which == ){
+      alert("G")
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
